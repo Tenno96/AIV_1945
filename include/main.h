5c3a6350a072f130dc3c6f9ca000991692fe4afb
Francesco Fusco <francescofusco1996@gmail.com>
2025-11-19 15:07:03 +0100

    first commit

---
+++ include/main.h
@@ -0,0 +1,98 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "raylib.h"
+
+typedef enum CollisionType
+{
+    Player = 1,
+    Enemy = 2,
+    EnemyBullet = 4,
+    PlayerBullet = 8
+}CollisionType;
+
+typedef struct ScrollableObject
+{
+    int posX;
+    int posY;
+    Texture2D* texture;
+    float speed;
+}ScrollableObject;
+
+typedef struct Animation2D
+{
+    int framesCount;
+    int currentFrame;
+    int framesCounter;
+    int framesSpeed;
+    bool isLooping;
+}Animation2D;
+
+typedef struct GameObject
+{
+    bool isActive;
+    Vector2 pos;
+    Vector2 pivotOffset;
+    Texture2D* texture;
+    Animation2D anim;
+    float speed;
+    unsigned char collisionMask;
+    CollisionType collisionType;
+}GameObject;
+
+typedef struct PlayerState
+{
+    int lifeCount;
+    int energy;
+    int score;
+}PlayerState;
+
+typedef struct Widget
+{
+    int posX;
+    int posY;
+    Texture2D texture;
+}Widget;
+
+
+
+void InitGame();
+void GameLoop();
+void UnloadGame();
+void ShoutDown();
+
+
+// Draw
+void DrawTiledArea(Texture2D texture, int startX, int startY, int tileCountX, int tileCountY);
+
+// Animation
+Animation2D CreateAnimation2D(int framesCount, int framesSpeed, bool isLooping);
+void UpdateAnimation2D(Animation2D* anim);
+
+// Player 
+PlayerState CreatePlayerState(int lifeCount, int energy, int score);
+void AddScore(PlayerState* playerState, int scoreToAdd);
+void RemoveEnergy(PlayerState* playerState, int quantityToRemove);
+void DecrementLifeCount(PlayerState* playerState);
+void FixPlayerPosition(GameObject* player);
+
+Vector2 GetInputDirection();
+
+// Physic
+void AddCollisionType(GameObject* object, CollisionType type);
+bool CollisionTypeMatches(GameObject* object, CollisionType type);
+
+void PlayerFire();
+
+void SetPosition(GameObject* object, Vector2 newPos);
+Vector2 GetPositionWithOffest(GameObject* object);
+GameObject* CreatePlayerBulletArray(int size, Texture2D texture);
+GameObject* GetAvailableGameobject(GameObject* objects, int poolSize);
+bool IsOutOfBounds(GameObject* object);
+
+
+// Enemy
+
+GameObject* CreateEnemy(int size, Texture2D texture);
+
+#endif // MAIN_H
---
+++ src/main.c
@@ -0,0 +1,366 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+//------------------------------------------------------------------------------------
+// Global Variables Declaration
+//------------------------------------------------------------------------------------
+
+static int screenWidth = 640;
+static int screenHeight = 480;
+
+static Texture2D backgroundTexture2D;
+static Texture2D playerTexture2D;
+static Texture2D playerBulletTexture2D;
+
+static GameObject player;
+static GameObject* playerBullets;
+
+
+int main(void)
+{
+    // Initialization
+    InitWindow(screenWidth, screenHeight, "AIV - 1945");
+    InitAudioDevice();
+    SetTargetFPS(60);
+
+    ChangeDirectory(GetApplicationDirectory());
+    TraceLog(LOG_DEBUG, "Change directory to: %s", GetApplicationDirectory());
+
+
+    InitGame();
+    GameLoop();
+    ShoutDown();
+
+    return 0;
+}
+
+void InitGame()
+{
+    backgroundTexture2D = LoadTexture("assets/map/water.png");
+    playerTexture2D = LoadTexture("assets/player/myplane_strip3.png");
+    playerBulletTexture2D = LoadTexture("assets/player/bullet.png");
+
+    player.texture = &playerTexture2D;
+    player.pos = (Vector2){ GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f };
+    player.pivotOffset = (Vector2){(float)playerTexture2D.height  * 0.5f, (float)playerTexture2D.height * 0.5f};
+    player.anim = CreateAnimation2D(3, 8, true);
+    player.speed = 150.0f;
+    player.collisionType = Player;
+    AddCollisionType(&player, Enemy);
+    AddCollisionType(&player, EnemyBullet);
+}
+
+void GameLoop()
+{
+    ScrollableObject background;
+    background.texture = &backgroundTexture2D;
+    background.posX = 0;
+    background.posY = 0;
+    background.speed = 80.0f;
+
+    ScrollableObject background2 = background; 
+    background2.posY -= GetScreenHeight();
+
+    playerBullets = CreatePlayerBulletArray(20, playerBulletTexture2D);
+
+    Widget bottomUI;
+    bottomUI.texture = LoadTexture("assets/ui/bottom.png");
+    bottomUI.posX = 0;
+    bottomUI.posY = GetScreenHeight() - bottomUI.texture.height;
+
+    // Music backgroundMC = LoadMusicStream("assets/audio/background.mp3");
+    // PlayMusicStream(backgroundMC);
+    // SetMusicVolume(backgroundMC, 0.25f);
+
+    while (!WindowShouldClose())
+    {
+        // INPUT
+        Vector2 inputDirection = GetInputDirection();
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            TraceLog(LOG_INFO, "Pressed fire button!");
+            GameObject* bullet = GetAvailableGameobject(playerBullets, 15);
+            bullet->pos = player.pos;
+            bullet->pos.y -= player.pivotOffset.y;
+        }
+
+        // UPDATE
+
+        // if (IsMusicStreamPlaying(backgroundMC))
+        // {
+        //     UpdateMusicStream(backgroundMC);
+        // }
+
+        background.posY += (int)(GetFrameTime() * 80.0f);
+        background2.posY += (int)(GetFrameTime() * 80.0f);
+
+        if (background.posY > GetScreenHeight())
+        {
+            background.posY -= GetScreenHeight() * 2;
+        }
+
+        if (background2.posY > GetScreenHeight())
+        {
+            background2.posY -= GetScreenHeight() * 2;
+        }
+
+        player.pos.x += (inputDirection.x * player.speed) * GetFrameTime();
+        player.pos.y += (inputDirection.y * player.speed) * GetFrameTime();
+
+        FixPlayerPosition(&player);
+
+        for (int i = 0; i < 15; i++)
+        {
+            if (playerBullets[i].isActive)
+            {
+                playerBullets[i].pos.y -= playerBullets[i].speed * GetFrameTime();
+                if (IsOutOfBounds(&playerBullets[i]))
+                {
+                    playerBullets[i].isActive = false;
+                }
+            }
+        }
+
+        // ANIMATIONS
+
+        UpdateAnimation2D(&player.anim);
+
+        // PHYSICS        
+
+        // DRAW
+
+        BeginDrawing();
+
+            ClearBackground(BLACK);
+
+            // Draw Background
+            DrawTiledArea(*background.texture, background.posX, background.posY, 20, 15);            
+            DrawTiledArea(*background2.texture, background2.posX, background2.posY, 20, 15);
+            
+            // Draw MiddleBackground
+
+            // Pawn
+
+            DrawTextureRec(*player.texture, (Rectangle){player.anim.currentFrame * 65, 0, 65, 65}, GetPositionWithOffest(&player), WHITE);
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (playerBullets[i].isActive)
+                {
+                    DrawTextureRec(*playerBullets[i].texture, (Rectangle){0, 0, 32, 32}, GetPositionWithOffest(&playerBullets[i]), WHITE);
+                }
+            }
+
+            DrawCircle((int)(GetScreenWidth() * 0.5f), (int)(GetScreenHeight() * 0.5f), 5, RED);
+
+            // HUD
+
+            DrawTexture(bottomUI.texture, bottomUI.posX, bottomUI.posY, WHITE);
+
+        EndDrawing();
+    }
+
+
+}
+
+void UnloadGame()
+{
+    UnloadTexture(backgroundTexture2D);
+    UnloadTexture(playerTexture2D);
+    UnloadTexture(playerBulletTexture2D);
+
+    free(playerBullets);
+}
+
+void ShoutDown()
+{
+    UnloadGame();
+    CloseWindow();
+}
+
+
+void DrawTiledArea(Texture2D texture, int startX, int startY, int tileCountX, int tileCountY)
+{
+    int tileWidth = texture.width;
+    int tileHeight = texture.height;
+
+    for (int y = 0; y < tileCountY; y++)
+    {
+        for (int x = 0; x < tileCountX; x++)
+        {
+            int posX = startX + (x * tileWidth);
+            int posY = startY + (y * tileHeight);
+
+            DrawTexture(texture, posX, posY, WHITE);
+        }
+    }
+}
+
+Animation2D CreateAnimation2D(int framesCount, int framesSpeed, bool isLooping)
+{
+    Animation2D anim;
+    anim.currentFrame = 0;
+    anim.framesCount = framesCount;
+    anim.framesCounter = 0;
+    anim.framesSpeed = framesSpeed;
+    anim.isLooping = isLooping;
+
+    return anim;
+}
+
+void UpdateAnimation2D(Animation2D* anim)
+{
+    anim->framesCounter++;
+
+    if (anim->framesCounter >= (60/anim->framesSpeed))
+    {
+        anim->framesCounter = 0;
+        anim->currentFrame++;
+
+        if (anim->currentFrame > anim->framesCount - 1)
+        {
+            anim->currentFrame = 0;
+        }
+    }
+}
+
+Vector2 GetInputDirection()
+{
+    Vector2 inputDirection = (Vector2){0, 0};
+
+    if (IsKeyDown(KEY_W))
+    {
+        inputDirection.y = -1;
+    }
+    else if (IsKeyDown(KEY_S))
+    {
+        inputDirection.y = 1;
+    }
+
+    if (IsKeyDown(KEY_A))
+    {
+        inputDirection.x = -1;
+    }
+    else if(IsKeyDown(KEY_D))
+    {
+        inputDirection.x = 1;
+    }
+
+    return inputDirection;
+}
+
+void AddCollisionType(GameObject* object, CollisionType type)
+{
+    object->collisionMask |= (unsigned char)type;
+}
+
+bool CollisionTypeMatches(GameObject* object, CollisionType type)
+{
+    return false;
+}
+
+Vector2 GetPositionWithOffest(GameObject* object)
+{
+    return (Vector2){ object->pos.x - object->pivotOffset.x, object->pos.y - object->pivotOffset.y };
+}
+
+GameObject* CreatePlayerBulletArray(int size, Texture2D texture)
+{
+    GameObject* array = (GameObject*)malloc(size * sizeof(GameObject));
+
+    if (array == NULL)
+    {
+        TraceLog(LOG_ERROR, "ERROR: memory allocation failed!\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        GameObject* gameObject = &array[i];
+        gameObject->pivotOffset = (Vector2){16, 16};
+        gameObject->isActive = false;
+        gameObject->texture = &texture;
+        gameObject->speed = 400.0f;
+        gameObject->collisionType = PlayerBullet;
+        AddCollisionType(gameObject, Enemy);
+        AddCollisionType(gameObject, EnemyBullet);
+    }
+
+    return array;
+}
+
+void FixPlayerPosition(GameObject* player)
+{
+    Vector2 pos_with_offset = GetPositionWithOffest(player);
+    
+    if (pos_with_offset.y + 10 < 0)
+    {
+        player->pos.y = player->pivotOffset.y - 10;
+    }
+    else if(pos_with_offset.y + player->pivotOffset.y + 25 > GetScreenHeight() - 76)
+    {
+        player->pos.y = ((GetScreenHeight() - 76) - (player->pivotOffset.y - 5));
+    }
+
+        if (pos_with_offset.x < 0)
+    {
+        player->pos.x = player->pivotOffset.x;
+    }
+    else if(pos_with_offset.x + player->pivotOffset.x + 25 > GetScreenWidth())
+    {
+        player->pos.x = (GetScreenWidth() - (player->pivotOffset.x - 5));
+    }
+}
+
+GameObject* GetAvailableGameobject(GameObject* objects, int poolSize)
+{
+    for (int i = 0; i < poolSize; i++)
+    {
+        if (objects[i].isActive) continue;
+
+        objects[i].isActive = true;
+        return &objects[i];
+    }
+
+    return NULL;
+}
+
+bool IsOutOfBounds(GameObject* object)
+{  
+    Vector2 pos_with_offset = GetPositionWithOffest(object);
+    if (pos_with_offset.y + object->pivotOffset.y >= 0 && pos_with_offset.y - object->pivotOffset.y <= GetScreenHeight() - 76)
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+GameObject* CreateEnemy(int size, Texture2D texture)
+{
+    GameObject* array = (GameObject*)malloc(size * sizeof(GameObject));
+
+    if (array == NULL)
+    {
+        TraceLog(LOG_ERROR, "ERROR: memory allocation failed!\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        GameObject gameObject;
+        gameObject.pivotOffset = (Vector2){16, 16};
+        gameObject.isActive = false;
+        gameObject.texture = &texture;
+        gameObject.speed = 400.0f;
+        gameObject.collisionType = Enemy;
+        AddCollisionType(&gameObject, Player);
+        AddCollisionType(&gameObject, PlayerBullet);
+
+        array[i] = gameObject;
+    }
+
+    return array;
+}
